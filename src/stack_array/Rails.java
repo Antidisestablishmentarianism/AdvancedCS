b4bbab2bc7ec7d8609e36f5e51eaad6e2071b35a
Antidisestablishmentarianism <saifkahmad@gmail.com>
2017-08-30 17:38:30 -0500

    Finish rails

--- src/stack_array/Rails.java
+++ src/stack_array/Rails.java
@@ -8,7 +8,7 @@ import java.io.IOException;
  * Created by 180502 on 8/29/2017.
  */
 public class Rails {
-    private static final String fileName = "C:\\Users\\180502\\IdeaProjects\\Stack\\src\\rails.txt";
+    private static final String fileName = "/Users/Saif/Documents/Repositories/projects/src/stack_array/rails";
 
     BufferedReader br;
     FileReader fr;
@@ -23,7 +23,34 @@ public class Rails {
 
         try {
             while ((currLine = br.readLine()) != null) {
-                //put logic here
+                String[] input = currLine.split(" ");
+
+                if (input.length <= 1) continue;
+
+                for (int i = input.length - 1; i >= 0; i--) {
+                    cars.push(Integer.parseInt(input[i]));
+                }
+
+                for (int i = 1; i <= input.length + 1; i++) {
+                    if (station.size() > 0) {
+                        if (station.peek() == cars.peek()) {
+                            station.pop();
+                            cars.pop();
+                            i--;
+                            continue;
+                        }
+                    }
+
+                    station.push(i);
+                }
+
+                if (cars.size() == 0)
+                    System.out.println("YES");
+                else
+                    System.out.println("NO");
+
+                cars.clear();
+                station.clear();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -40,12 +67,6 @@ public class Rails {
         try {
             fr = new FileReader(fileName);
             br = new BufferedReader(fr);
-
-            String currLine;
-
-            while ((currLine = br.readLine()) != null) {
-
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
