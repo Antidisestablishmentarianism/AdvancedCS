9642806a2f05572b86a5b16bf5e8056c0aa3f969
Antidisestablishmentarianism <saifkahmad@gmail.com>
2017-08-30 18:01:58 -0500

    Add helper class to read files

---
+++ src/helper/Streamer.java
@@ -0,0 +1,41 @@
+package helper;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by Saif on 8/30/17.
+ */
+public class Streamer {
+    private BufferedReader br;
+    private FileReader fr;
+
+    public Streamer(String fileName) {
+        try {
+            fr = new FileReader(fileName);
+            br = new BufferedReader(fr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void closeStream() {
+        try {
+            if (br != null) br.close();
+            if (fr != null) fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/stack_array/Rails.java
+++ src/stack_array/Rails.java
@@ -1,83 +1,55 @@
 package stack_array;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import helper.Streamer;
 
 /**
- * Created by 180502 on 8/29/2017.
+ * Created by Saif on 8/29/2017.
  */
 public class Rails {
     private static final String fileName = "/Users/Saif/Documents/Repositories/projects/src/stack_array/rails";
 
-    BufferedReader br;
-    FileReader fr;
+    Streamer streamer;
 
     ArrayStack<Integer> cars = new ArrayStack<>();
     ArrayStack<Integer> station = new ArrayStack<>();
 
     public Rails() {
-        openStream();
+        streamer = new Streamer(fileName);
 
         String currLine;
 
-        try {
-            while ((currLine = br.readLine()) != null) {
-                String[] input = currLine.split(" ");
+        while ((currLine = streamer.readLine()) != null) {
+            String[] input = currLine.split(" ");
 
-                if (input.length <= 1) continue;
+            if (input.length <= 1) continue;
 
-                for (int i = input.length - 1; i >= 0; i--) {
-                    cars.push(Integer.parseInt(input[i]));
-                }
-
-                for (int i = 1; i <= input.length + 1; i++) {
-                    if (station.size() > 0) {
-                        if (station.peek() == cars.peek()) {
-                            station.pop();
-                            cars.pop();
-                            i--;
-                            continue;
-                        }
-                    }
-
-                    station.push(i);
-                }
-
-                if (cars.size() == 0)
-                    System.out.println("YES");
-                else
-                    System.out.println("NO");
-
-                cars.clear();
-                station.clear();
+            for (int i = input.length - 1; i >= 0; i--) {
+                cars.push(Integer.parseInt(input[i]));
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            for (int i = 1; i <= input.length + 1; i++) {
+                if (station.size() > 0) {
+                    if (station.peek() == cars.peek()) {
+                        station.pop();
+                        cars.pop();
+                        i--;
+                        continue;
+                    }
+                }
+
+                station.push(i);
+            }
+
+            System.out.println(cars.size() == 0 ? "YES" : "NO");
+
+            cars.clear();
+            station.clear();
         }
 
-        closeStream();
+        streamer.closeStream();
     }
 
     public static void main(String[] args) {
         new Rails();
-    }
-
-    public void openStream() {
-        try {
-            fr = new FileReader(fileName);
-            br = new BufferedReader(fr);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void closeStream() {
-        try {
-            if (br != null) br.close();
-            if (fr != null) fr.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
