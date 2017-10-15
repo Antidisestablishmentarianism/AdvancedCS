13fb45429afe8d03d9da343707d21e2346169aab
the-saif-ahmad <saifkahmad@gmail.com>
2017-10-14 20:02:44 -0500

    Start writing stack for word ladder

--- src/linked_list/word_ladder/WordNode.java
+++ src/linked_list/word_ladder/WordNode.java
@@ -6,4 +6,42 @@ package linked_list.word_ladder;
 public class WordNode {
     private String data;
 
+	private WordNode next, prev;
+
+	public WordNode() {
+		data = null;
+		next = null;
+		prev = null;
+	}
+
+	public WordNode(String data) {
+		this.data = data;
+
+		next = null;
+		prev = null;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String value) {
+		data = value;
+	}
+
+	public WordNode getNext() {
+		return next;
+	}
+
+	public void setNext(WordNode value) {
+		next = value;
+	}
+
+	public WordNode getPrev() {
+		return prev;
+	}
+
+	public void setPrev(WordNode value) {
+		prev = value;
+	}
 }
